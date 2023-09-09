28a60e17413548ded2a34674f87a4f269a2e7de4
Raktim Parasar <raktimparasar96@gmail.com>
2023-09-09 16:53:09 +0530

    servers page ui ⚒️

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,5 @@
+const ServerPage = () => {
+  return <div>page</div>;
+};
+
+export default ServerPage;
--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: Request) {
         },
       },
     });
-    console.log("res....>>", NextResponse.json(server));
+
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVER_POST]", error);
