0ea17b2052e4fb43e1b68eb9f1e770eedf2ca2b9
Raktim Parasar <raktimparasar96@gmail.com>
2023-09-09 17:23:43 +0530

    adding tooltip component

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+type ActionTooltipProps = {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "right" | "left";
+  align?: "start" | "center" | "end";
+};
+
+export const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
