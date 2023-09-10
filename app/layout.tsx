ee36bdba77b479a611d792ea08b3614111d6cdc8
Raktim Parasar <raktimparasar96@gmail.com>
2023-09-10 21:39:10 +0530

    creating multiple servers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -29,6 +29,7 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
